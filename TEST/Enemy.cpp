a53d8d2f5cb50dbdbd95952e2f4b1e6430adba6f
[ymRamia0131] <[k019g1373@g.neec.ac.jp]>
2022-11-11 11:24:22 +0900

    Enemyの向きの取得/Enemyの画像の更新

--- TEST/Enemy.cpp
+++ TEST/Enemy.cpp
@@ -12,21 +12,41 @@ void Enemy::Initialize()
 {
 	LoadTexture();
 	position_X = 1000.0f;
-	position_Y = 700.0f;
+	position_Y = 600.0f;
 	count = 0;
 	speed = 5.f;
 }
 
 void Enemy::Update()
 {
-	DrawGraph(position_X, position_Y, Etexture, FALSE);
+	SetDrawBlendMode(DX_BLENDMODE_ALPHA, 128);
+	DrawBox(position_X - 250, position_Y, position_X + 450, position_Y + 192,GetColor(255,0,0),true);
+	SetDrawBlendMode(DX_BLENDMODE_NOBLEND, 0);
+	DrawGraph(position_X, position_Y, Etexture, TRUE);
 	count++;
 	position_X += speed;
 
 	if (count == 64)
 	{
+		beforSpeed = speed;
+		speed = 0;
+	}
+	else if (count == 128)
+	{
+		speed = beforSpeed;
 		speed *= -1;
 		count = 0;
+	}
+
+	if (speed > 0)
+	{
+		flont = 0;
+		Etexture = LoadGraph("Resources/Enemy/01_Enemy_R.png");
+	}
+	else if (speed < 0)
+	{
+		flont = 1;
+		Etexture = LoadGraph("Resources/Enemy/01_Enemy_L.png");
 	}
 }
 
@@ -58,5 +78,5 @@ int Enemy::SetDeath(int d)
 
 void Enemy::LoadTexture()
 {
-	Etexture = LoadGraph("Resources/Enemy/Enemy.png");
+	Etexture = LoadGraph("Resources/Enemy/01_Enemy_R.png");
 }
