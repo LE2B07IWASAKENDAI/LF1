02fb0c039c6a44a20b5a33421a16bbf21170d7ce
[Aya-Nattu] <[hanamaru.rururu870@gmail.com]>
2022-11-18 09:13:23 +0900

    knifeクラス作成

---
+++ TEST/knife.cpp
@@ -0,0 +1,26 @@
+#include "knife.h";
+
+knife::knife()
+{
+
+}
+
+knife::~knife()
+{
+
+}
+
+void knife::Initialize()
+{
+
+}
+
+void knife::Update()
+{
+
+}
+
+void knife::Draw()
+{
+
+}
---
+++ TEST/knife.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "DxLib.h";
+#include <vector>;
+
+using namespace std;
+
+class knife
+{
+public:
+	knife();
+	~knife();
+	void Initialize();
+	void Update();
+	void Draw();
+
+};
