011369939a77a06bc3db1d06edb6e136bb76c835
[ymRamia0131] <[k019g1373@g.neec.ac.jp]>
2022-12-19 15:04:14 +0900

    左右両方を見るenemyクラスを追加

---
+++ TEST/LandREnemy.cpp
@@ -0,0 +1,124 @@
+#include "LandREnemy.h"
+
+LandREnemy::LandREnemy()
+{
+
+}
+
+LandREnemy::~LandREnemy()
+{
+
+}
+
+void LandREnemy::Initialize()
+{
+	LoadTexture();
+	flont = 0;
+	death = 0;
+	//count = 0;
+	//checkCount = 0;
+	//‰¼À•W(Œˆ‚ß‘Å‚¿)
+	//position_X = 500;
+	//position_Y = 500;
+}
+
+void LandREnemy::Horizon()
+{
+	SetDrawBlendMode(DX_BLENDMODE_ALPHA, 128);
+
+	if (flont == 1)
+	{
+		LandREtexture = LoadGraph("Resources/Enemy/01_Enemy_R.png");
+		//DrawBox(GetPosition_X(), position_Y, GetPosition_X() + 450, position_Y + 192, GetColor(255, 0, 0), true);
+		DrawBox(position_X, position_Y, position_X + 450, position_Y + 192, GetColor(255, 0, 0), true);
+	}
+	else if (flont == 0)
+	{
+		LandREtexture = LoadGraph("Resources/Enemy/01_Enemy_L.png");
+		//DrawBox(GetPosition_X() - 250, position_Y, GetPosition_X() + 192, position_Y + 192, GetColor(255, 0, 0), true);
+		DrawBox(position_X - 250, position_Y, position_X + 192, position_Y + 192, GetColor(255, 0, 0), true);
+	}
+	SetDrawBlendMode(DX_BLENDMODE_NOBLEND, 0);
+
+}
+
+void LandREnemy::Dead()
+{
+	SetDeath(1);
+}
+
+void LandREnemy::Monitoring()
+{
+	switch (Mode)
+	{
+	case Normal:
+		break;
+	case Check:
+		if (position_X > BreakPoint)
+		{
+			flont = 0;
+			if (EqualFlag == 0)
+			{
+				speed_ = -5;
+			}
+		}
+		if (position_X < BreakPoint)
+		{
+			flont = 1;
+			if (EqualFlag == 0)
+			{
+				speed = 5;
+			}
+		}
+
+		if (position_X + 10 > BreakPoint && position_X - 10 < BreakPoint)
+		{
+			speed = 0;
+			speed_ = 0;
+			EqualFlag = 1;
+		}
+
+		if (EqualFlag == 1)
+		{
+			Mode = Normal;
+		}
+		break;
+	}
+}
+
+void LandREnemy::Update()
+{
+	if (GetDeath() == 0) {
+		Horizon();
+		Monitoring();
+	}
+}
+
+void LandREnemy::LoadTexture()
+{
+	LandREtexture = LoadGraph("Resources/Enemy/01_Enemy_R.png");
+}
+
+void LandREnemy::Draw()
+{
+	if (GetDeath() == 0) {
+		DrawGraph(position_X, position_Y, LandREtexture, TRUE);
+	}
+	//DebugLog();
+}
+
+void LandREnemy::Set_position(float position_x, float position_y)
+{
+	position_X = position_x;
+	position_Y = position_y;
+}
+
+void LandREnemy::Breaking(float x)
+{
+	BreakPoint = x;
+}
+
+void LandREnemy::CheckSound()
+{
+	Mode = Check;
+}
---
+++ TEST/LandREnemy.h
@@ -0,0 +1,72 @@
+#pragma once
+
+#include"DxLib.h"
+
+class LandREnemy
+{
+public:
+	LandREnemy();
+	~LandREnemy();
+
+	//初期化
+	void Initialize();
+	//視野
+	void Horizon();
+	//死亡判定処理
+	void Dead();
+	//毎フレーム更新
+	void Update();
+	//監視
+	void Monitoring();
+
+	//画像読み込み
+	void LoadTexture();
+	//描画
+	void Draw();
+	//デバッグ表示
+	//void DebugLog();
+
+	/*Getter,Setter*/
+	//座標値
+	void Set_position(float position_x, float position_y);
+
+	float GetPosition_X() { return position_X; }
+	float Getposition_Y() { return position_Y; }
+
+	//生死判定
+	int GetDeath() { return death; }
+	void SetDeath(int d) { death = d; }
+
+	//向いている方向
+	int GetFlont() { return flont; }
+
+	void CheckSound();
+	void Breaking(float x);
+	int Mode;
+	enum Mode
+	{
+		Normal,
+		Check,
+	};
+
+public:
+	//グラフィックハンドル
+	int LandREtexture;
+	//座標
+	float position_X = 0.0f;
+	float position_Y = 0.0f;
+
+	int death = 0; //生死　0:生きている 1:死んでいる
+	int flont = 0;//0:右　1:左
+	//花瓶の割れた位置取得
+	float BreakPoint = 0;
+	int EqualFlag = 0; //0:未到達　1:到達
+
+private:
+	//右、左に進む速度
+	float speed = 5.0f;
+	float speed_ = -5.0f;
+	//移動量
+	float vector = 0.0f;
+};
+
